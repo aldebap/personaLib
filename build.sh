c88f6930f8e6cbe4274f1a261a513fd0e43443c0
aldebap <aldeba_bla@yahoo.com>
2022-02-26 23:49:24 -0300

    Refactoring CLI arg parsing in build script

--- build.sh
+++ build.sh
@@ -1,81 +1,220 @@
 #!	/usr/bin/ksh
 
+###############################################################################
+#	build a golang project described by a build config file
+################################################################################
+
 #	color constants
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-LIGHTGRAY='\033[0;37m'
-NOCOLOR='\033[0m'
+export	RED='\033[0;31m'
+export	GREEN='\033[0;32m'
+export	LIGHTGRAY='\033[0;37m'
+export	NOCOLOR='\033[0m'
 
 #	set environment
-export  PROJECT_CONFIG_FILE='build.cfg'
-export  TARGETDIR='bin'
 
-#	TODO: create an option to set the target dir
+export	PROJECT_CONFIG_FILE='build.cfg'
+export	TARGET_DIR='bin'
+export	VERBOSE='false'
+export	TARGET_LIST=''
 
-#   check if go toolchain is available
-if [ -z "${GOROOT}" ]
-then
-    exit "error: GOROOT environment variable must be set"
-fi
+#	function to check if the required tools are available
+function checkRequiredTools {
 
-OUTPUT=$( which go 2> /dev/null )
-if [ $? -ne 0 ]
-then
-    exit "error: 'go' compiler is required to run this script"
-fi
+	local	TMP_OUTPUT=''
 
-#   check if jq is available
-OUTPUT=$( which jq 2> /dev/null )
-if [ $? -ne 0 ]
-then
-    exit "error: 'jq' is required to run this script"
-fi
+	#	if go toolchain is available
+	if [ -z "${GOROOT}" ]
+	then
+		"error: GOROOT environment variable must be set"
+	fi
 
-#	check if config file exists
-if [ ! -f ${PROJECT_CONFIG_FILE} ]
-then
-    exit "error: build configration file not found: ${PROJECT_CONFIG_FILE}"
-fi
+	TMP_OUTPUT=$( which go 2> /dev/null )
+	if [ $? -ne 0 ]
+	then
+		exit "error: 'go' compiler is required to run this script"
+	fi
 
-#	load configuration from config file
-export	PROJECT_CONFIG="$( cat ${PROJECT_CONFIG_FILE} )"
-export  PROJECT_NAME="$( echo ${PROJECT_CONFIG} | jq '.project' | tr -d '"' )"
-export  PROJECT_DESCRIPTION="$( echo ${PROJECT_CONFIG} | jq '.description' | tr -d '"' )"
-export  PROJECT_SOURCEDIR="$( echo ${PROJECT_CONFIG} | jq '.source_directory' | tr -d '"' )"
-export  PROJECT_SOURCE="$( echo ${PROJECT_CONFIG} | jq '.source | .[]' | tr -d '"' )"
-export  PROJECT_PACKAGES="$( echo ${PROJECT_CONFIG} | jq '.packages | .[]' | tr -d '"' )"
-export	PROJECT_TARGET="$( echo ${PROJECT_CONFIG} | jq '.target' | tr -d '"' )"
-export  PROJECT_DEPENDENCIES="$( echo ${PROJECT_CONFIG} | jq '.dependencies | .[]' | tr -d '"' )"
+	#	 if jq is available
+	OUTPUT=$( which jq 2> /dev/null )
+	if [ $? -ne 0 ]
+	then
+		exit "error: 'jq' is required to run this script"
+	fi
+}
 
-#	TODO: validate the values from config file
+#	function to load project configuration from config file
+function loadProjectConfig {
 
-#	TODO: set default values for optional config variables
+	local	PROJECT_CONFIG="$( cat ${PROJECT_CONFIG_FILE} )"
 
-#	if necessary, create target directory
-if [ ! -d "${TARGETDIR}" ]
-then
-	mkdir "${TARGETDIR}"
-fi
+	export	PROJECT_NAME="$( echo ${PROJECT_CONFIG} | jq '.project' | tr -d '"' )"
+	export	PROJECT_DESCRIPTION="$( echo ${PROJECT_CONFIG} | jq '.description' | tr -d '"' )"
+	export	PROJECT_SOURCE_DIR="$( echo ${PROJECT_CONFIG} | jq '.source_directory' | tr -d '"' )"
+	export	PROJECT_SOURCE="$( echo ${PROJECT_CONFIG} | jq '.source | .[]' | tr -d '"' )"
+	export	PROJECT_PACKAGES="$( echo ${PROJECT_CONFIG} | jq '.packages | .[]' | tr -d '"' )"
+	export	PROJECT_TARGET="$( echo ${PROJECT_CONFIG} | jq '.target' | tr -d '"' )"
+	export	PROJECT_DEPENDENCIES="$( echo ${PROJECT_CONFIG} | jq '.dependencies | .[]' | tr -d '"' )"
 
-#	if necessary, set default target
-export	TARGET_LIST="$*"
+	#	TODO: validate the values from config file
+	#	TODO: set default values for optional config variables
+	#	TODO: change the config file to allow specify custom golang compilation options
+	#	TODO: change the config file to allow specify the version of dependency packages
+}
 
-if [ -z "${TARGET_LIST}" ]
-then
-	export	TARGET_LIST='compile'
-fi
+#	function to execute the "clean" target action
+function cleanTarget {
 
-#	TODO: create a better CLI option parser
-if [ "${TARGET_LIST}" == 'all' ]
-then
-	export	TARGET_LIST='dependencies compile test package verify'
-fi
+	#	TODO: is it possible to remove a package installed in GOPATH dir ?
+	#rm -rf pkg/*
+	echo -e '[debug]' ${LIGHTGRAY} rm -rf "${PROJECT_SOURCE_DIR}/go.mod" "${PROJECT_SOURCE_DIR}/go.sum" ${NOCOLOR}
 
-#	help message
-if [ "${TARGET_LIST}" == 'help' -o "${TARGET_LIST}" == '--help' ]
-then
-	cat <<HELP_MESSAGE
-$( basename ${0} ): specify one or more targets:
+	for PACKAGE in ${PROJECT_PACKAGES}
+	do
+		echo -e cd "${PACKAGE}"
+		echo -e rm -rf "${PROJECT_SOURCE_DIR}/go.mod" "${PROJECT_SOURCE_DIR}/go.sum"
+	done
+
+	echo -e rm -f "${TARGET_DIR}/${PROJECT_TARGET}"
+}
+
+#	function to execute the "dependencies" target action
+function dependenciesTarget {
+
+	#	TODO: adjust the target
+	#	main packge dependencies
+	cd src
+	go mod init aldebap/${PROJECT_NAME}
+
+	for MODULE in "${PROJECT_DEPENDENCIES}"
+	do
+		go get -u ${MODULE}
+	done
+
+	for PACKAGE in "${PROJECT_PACKAGES}"
+	do
+		echo "require ${PROJECT_NAME}/${PACKAGE} v0.0.0-unpublished" >> go.mod
+		echo "replace ${PROJECT_NAME}/${PACKAGE} v0.0.0-unpublished => ../src/${PACKAGE}" >> go.mod
+		#echo go mod edit -replace ${PROJECT_NAME}/${PACKAGE}=../src/${PACKAGE}
+	done
+
+	cd ..
+
+	#	create project packages module files
+	for PACKAGE in ${PROJECT_PACKAGES}
+	do
+		cd src/${PACKAGE}
+		go mod init ${PROJECT_NAME}/${PACKAGE}
+		cd ../..
+	done
+}
+
+#	function to execute the "compile" target action
+function compileTarget {
+
+	local	BUILD_DIR="$( pwd )"
+
+	rm -f "${TARGET_DIR}/${PROJECT_TARGET}"
+
+	if [ ! -z "${PROJECT_SOURCE_DIR}" -a "${PROJECT_SOURCE_DIR}" != '.' ]
+	then
+		cd "${PROJECT_SOURCE_DIR}"
+	fi
+	go build -o "${BUILD_DIR}/${TARGET_DIR}/${PROJECT_TARGET}" ${PROJECT_SOURCE}
+	cd "${BUILD_DIR}"
+}
+
+#	function to execute the "test" target action
+function testTarget {
+
+	local	BUILD_DIR="$( pwd )"
+	local	TEST_RESULT=0
+	local	GOTEST_FLAGS=''
+
+	if [ ! -z "${PROJECT_SOURCE_DIR}" -a "${PROJECT_SOURCE_DIR}" != '.' ]
+	then
+		cd "${PROJECT_SOURCE_DIR}"
+	fi
+
+	if [ "${VERBOSE}" == 'true' ]
+	then
+		GOTEST_FLAGS='-v'
+	fi
+
+	go test "${GOTEST_FLAGS}" .
+	if [ $? -ne 0 ]
+	then
+		export	TEST_RESULT=1
+	fi
+
+	for PACKAGE in ${PROJECT_PACKAGES}
+	do
+		echo cd "${PACKAGE}"
+		cd "${PACKAGE}"
+		go test "${GOTEST_FLAGS}" .
+		if [ $? -ne 0 ]
+		then
+			export	TEST_RESULT=1
+		fi
+		cd ..
+	done
+
+	if [ ${TEST_RESULT} -ne 0 ]
+	then
+		echo -e "[build] ${RED}unit tests failed${NOCOLOR}"
+		exit ${TEST_RESULT}
+	fi
+
+	cd "${BUILD_DIR}"
+}
+
+#	function to execute the "package" target action
+function packageTarget {
+
+	#	TODO: adjust the target
+	docker build --tag shopping-cart .
+}
+
+#	function to execute the "verify" target action
+function verifyTarget {
+
+	#	TODO: adjust the target
+	docker-compose up -d
+	newman run 'test/Integrated Tests.postman_collection.json' --environment 'test/Localhost.postman_environment.json'
+	docker-compose stop
+}
+
+#	function to execute the "run" target action
+function runTarget {
+
+	#	TODO: adjust the target
+	docker-compose up
+}
+
+#	CLI arguments parsing
+
+while [ true ]
+do
+	ARG=${1}
+
+	if [ -z "${ARG}" ]
+	then
+		break
+	fi
+
+	case ${ARG} in
+
+	#	help message option
+		--help )
+		cat <<HELP_MESSAGE
+$( basename ${0} ): [options] targets
+options:
+	--config-file file-name: set the project config file name (default: ${PROJECT_CONFIG_FILE})
+	--target-dir: set the target directory name (default: ${TARGET_DIR})
+	--verbose: show detailed information during execution
+	--help: show this help message
+
+targets:
+	all: the same as the specifying targets "dependencies compile test package verify"
 	clean: remove all required packages and the target file
 	dependencies: install all dependencies
 	compile: compile and link source code
@@ -85,145 +224,126 @@ $( basename ${0} ): specify one or more targets:
 	run: run the target
 HELP_MESSAGE
 
-	exit 0
+		exit 0
+		;;
+
+	#	config file name option
+		--config-file )
+		PROJECT_CONFIG_FILE=${2}
+		shift
+		;;
+
+	#	target directory name option
+		--target-dir )
+		TARGET_DIR=${2}
+		shift
+		;;
+
+	#	verbose option
+		--verbose )
+		VERBOSE='true'
+		shift
+		;;
+
+	#	if it's not an option, it's a target
+		* )
+		if [ -z "${TARGET_LIST}" ]
+		then
+			TARGET_LIST="${ARG}"
+		else
+			TARGET_LIST="${TARGET_LIST} ${ARG}"
+		fi
+		;;
+	esac
+
+	shift
+done
+
+#	check if config file exists
+if [ -z "${PROJECT_CONFIG_FILE}" -o ! -f ${PROJECT_CONFIG_FILE} ]
+then
+	echo -e "[build] ${RED}error: build configration file not found: \"${PROJECT_CONFIG_FILE}\"${NOCOLOR}"
+	exit 1
 fi
 
-#	perform every specified target
+#	if necessary, create target directory
+if [ ! -d "${TARGET_DIR}" ]
+then
+	mkdir "${TARGET_DIR}"
+fi
+
+#	if necessary, set default target
+if [ -z "${TARGET_LIST}" ]
+then
+	export	TARGET_LIST='compile'
+fi
+
+if [ "${TARGET_LIST}" == 'all' ]
+then
+	export	TARGET_LIST='dependencies compile test package verify'
+fi
+
+#	load project config files and perform every specified target
+checkRequiredTools
+loadProjectConfig
+
 for TARGET in ${TARGET_LIST}
 do
-	case ${TARGET} in 
+	case ${TARGET} in
 
-	#	remove all required packages and the target file
+		#	remove all required packages and the target file
 		clean )
-		echo -e "[build] ${GREEN}removing required packages and the target file${NOCOLOR}"
+		echo -e "[build] ${TARGET}: ${GREEN}removing required packages and the target file${NOCOLOR}"
 
-		#	TODO: is it possible to remove a package installed in GOPATH dir ?
-		#rm -rf pkg/*
-		echo -e '[debug]' ${LIGHTGRAY} rm -rf "${PROJECT_SOURCEDIR}/go.mod" "${PROJECT_SOURCEDIR}/go.sum" ${NOCOLOR}
-
-		for PACKAGE in ${PROJECT_PACKAGES}
-		do
-			echo -e cd "${PACKAGE}"
-			echo -e rm -rf "${PROJECT_SOURCEDIR}/go.mod" "${PROJECT_SOURCEDIR}/go.sum"
-		done
-
-		echo -e rm -f "${TARGETDIR}/${PROJECT_TARGET}"
+		cleanTarget
 		;;
 
-	#	install all dependencies
+		#	install all dependencies
 		dependencies )
-		echo '+++ downloading and installing all dependencies'
+		echo -e "[build] ${TARGET}: ${GREEN}downloading and installing all dependencies${NOCOLOR}"
 
-		#	TODO: adjust the target
-		#	main packge dependencies
-		cd src
-		go mod init aldebap/${PROJECT_NAME}
-
-		for MODULE in "${PROJECT_DEPENDENCIES}"
-		do
-			go get -u ${MODULE}
-		done
-
-		for PACKAGE in "${PROJECT_PACKAGES}"
-		do
-			echo "require ${PROJECT_NAME}/${PACKAGE} v0.0.0-unpublished" >> go.mod
-			echo "replace ${PROJECT_NAME}/${PACKAGE} v0.0.0-unpublished => ../src/${PACKAGE}" >> go.mod
-			#echo go mod edit -replace ${PROJECT_NAME}/${PACKAGE}=../src/${PACKAGE}
-		done
-
-		cd ..
-
-		#	create project packages module files
-		for PACKAGE in ${PROJECT_PACKAGES}
-		do
-			cd src/${PACKAGE}
-			go mod init ${PROJECT_NAME}/${PACKAGE}
-			cd ../..
-		done
+		dependenciesTarget
 		;;
 
-	#	compile and link source code
+		#	compile and link source code
 		compile )
-		echo -e "[build] ${GREEN}compiling and linking source code${NOCOLOR}"
+		echo -e "[build] ${TARGET}: ${GREEN}compiling and linking source code${NOCOLOR}"
 
-		rm -f "${TARGETDIR}/${PROJECT_TARGET}"
-
-		BUILD_DIR="$( pwd )"
-		if [ ! -z "${PROJECT_SOURCEDIR}" -a "${PROJECT_SOURCEDIR}" != '.' ]
-		then
-			cd "${PROJECT_SOURCEDIR}"
-		fi
-		go build -o "${BUILD_DIR}/${TARGETDIR}/${PROJECT_TARGET}" ${PROJECT_SOURCE}
-		cd "${BUILD_DIR}"
+		compileTarget
 		;;
 
-	#	execute unit tests
+		#	execute unit tests
 		test )
-		echo -e "[build] ${GREEN}running unit tests${NOCOLOR}"
+		echo -e "[build] ${TARGET}: ${GREEN}running unit tests${NOCOLOR}"
 
-		export	TEST_RESULT=0
-
-		BUILD_DIR="$( pwd )"
-		if [ ! -z "${PROJECT_SOURCEDIR}" -a "${PROJECT_SOURCEDIR}" != '.' ]
-		then
-			cd "${PROJECT_SOURCEDIR}"
-		fi
-
-		#	TODO: the verbose for the unit tests should be a CLI option
-		go test -v .
-		if [ $? -ne 0 ]
-		then
-			export	TEST_RESULT=-1
-		fi
-
-		for PACKAGE in ${PROJECT_PACKAGES}
-		do
-			echo cd "${PACKAGE}"
-			cd "${PACKAGE}"
-			go test -v .
-			if [ $? -ne 0 ]
-			then
-				export	TEST_RESULT=-1
-			fi
-			cd ..
-		done
-
-		if [ ${TEST_RESULT} -ne 0 ]
-		then
-			exit ${TEST_RESULT}
-		fi
-
-		cd "${BUILD_DIR}"
+		testTarget
 		;;
 
-	#	package the target in a Docker image
+		#	package the target in a Docker image
 		package )
-		echo '+++ package the target in a Docker image'
-		#	TODO: adjust the target
-		docker build --tag shopping-cart .
+		echo -e "[build] ${TARGET}: ${GREEN}package the target in a Docker image${NOCOLOR}"
+
+		packageTarget
 		;;
 
-	#	execute integration tests
+		#	execute integration tests
 		verify )
-		echo '+++ execute integration tests'
+		echo -e "[build] ${TARGET}: ${GREEN}execute integration tests${NOCOLOR}"
 
-		#	TODO: adjust the target
-		docker-compose up -d
-		newman run 'test/Integrated Tests.postman_collection.json' --environment 'test/Localhost.postman_environment.json'
-		docker-compose stop
+		verifyTarget
 		;;
 
-	#	run the target
+		#	run the target
 		run )
-		echo '+++ run the target'
+		echo -e "[build] ${TARGET}: ${GREEN}running the project${NOCOLOR}"
 
-		#	TODO: adjust the target
-		docker-compose up
+		runTarget
 		;;
 
-	#	unknown target
+		#	unknown target
 		* )
-		echo "[error] unknown target ${TARGET}"
+		echo -e "[build] ${RED}error: unknown target ${TARGET}${NOCOLOR}"
+		exit 1
 		;;
 	esac
 done
